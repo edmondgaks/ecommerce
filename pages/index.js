a72f6e0fe7d45c1b417ae09d7b9a5e66c74a3229
Edmond GAKS <edmondgaks@gmail.com>
2022-09-25 14:13:17 +0200

    getting and using serversideProps

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React from 'react';
+import { client } from '../lib/client';
 import { Product, FooterBanner, HeroBanner } from '../components';
-const Home = () => {
+const Home = ({ products,bannerData}) => {
   return (
     <>
     <HeroBanner />
@@ -9,11 +10,20 @@ const Home = () => {
       <p>Speakers of many variations</p>
     </div>
     <div className="products-container">
-      {['Product1', 'Product 2'].map((product) => product)}
+      {products?.map((product) => product.name)}
     </div>
     <FooterBanner />
     </>
   )
 }
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
 
+  return {
+    props: { products,bannerData }
+  }
+}
 export default Home
