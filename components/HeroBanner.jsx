7b213d6de0766215e117fdc2fba806f8ba0141c6
Edmond GAKS <edmondgaks@gmail.com>
2022-09-27 19:38:15 +0200

    working on the left sidebar

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React from 'react';
 import Link from 'next/link';
 import { urlFor } from '../lib/client';
+
 const HeroBanner = ({ heroBanner }) => {
     return (
         <div className="hero-banner-container">
--- context/stateContext.js
+++ context/stateContext.js
@@ -1,13 +1,12 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import { toast } from 'react-hot-toast';
-
 const Context = createContext();
 
 export const StateContext = ({ children }) => {
     const [showCart, setshowCart] = useState(false);
     const [cartItems,setcartItems] = useState([]);
     const [totalPrice, settotalPrice] = useState();
-    const [totalQuantities, settotalQuantities] = useState();
+    const [totalQuantities, settotalQuantities] = useState(0);
     const [qty, setqty] = useState(1);
     const incQty = () => {
         setqty((prevQty) => prevQty + 1);
@@ -19,16 +18,16 @@ export const StateContext = ({ children }) => {
         });
     }
 
-    const onAdd = (product,quantiy) => {
+    const onAdd = (product,quantity) => {
         const checkProductInCart = cartItems.find((item) => item._id === product._id);
         if(checkProductInCart) {
-            settotalPrice((prevTotalPrice) => prevTotalPrice + product.price * quantiy);
-            settotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantiy);
+            settotalPrice((prevTotalPrice) => prevTotalPrice + product.price * quantity);
+            settotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity);
 
             const updatedCartItems = cartItems.map((cartProduct) => {
                 if(cartProduct._id === product._id) return {
                     ...cartProduct,
-                    quantiy: cartProduct.quantiy + quantity
+                    quantity: cartProduct.quantiy + quantity
                 }
             })
             setcartItems(updatedCartItems);
@@ -42,7 +41,7 @@ export const StateContext = ({ children }) => {
 
     return (
         <Context.Provider value={{
-            cartItems,showCart,totalPrice,totalQuantities,qty,setqty,incQty,decQty, onAdd
+            cartItems,showCart,totalPrice,totalQuantities,qty,setqty,incQty,decQty, onAdd, setshowCart
         }}>
             {children}
         </Context.Provider>
