8c08f1e0f8a76610f49fc06a635ef5a1a7414795
Edmond GAKS <edmondgaks@gmail.com>
2022-10-02 17:51:37 +0200

    adding the former stripe variables

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -12,7 +12,17 @@ const Cart = () => {
     const { totalPrice, totalQuantities, cartItems, setshowCart, toggleCartItemQuantity, onRemove } = useStateContext();
     const handleCheckout = async () => {
         const stripe = await getStripe();
-        const response = await fetch('/api/stripe');
+        const response = await fetch('/api/stripe', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(cartItems),
+        });
+        if(response.statusCode === 500) return;
+        const data = await response.json();
+        toast.loading('Redirecting...');
+        stripe.redirectToCheckout({ sessionId: data.id });
 
     }
     return (
--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,9 +1,10 @@
 import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env. STRIPE_SECRET_KEY);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
  
 export default async function handler(req,res) {
     if(req.method === 'POST') {
+        console.log(req.body.cartItems)
         try {
             const params = {
                 submit_type: 'pay',
@@ -14,18 +15,31 @@ export default async function handler(req,res) {
                     { shipping_rate: ''},
                     { shipping_rate: ''}, 
                 ],
-                line_items: [
-                    {
-                        price: '{{PRICE_ID}}',
-                        quantity: 1,
+                line_items: req.body.cartItems.map((item) => {
+                    const img = item.image[0].asset._ref;
+                    const newImage = img.replace('image-', 'https://cdn.sanity.io/images/v2tkzg4u/production/').replace('-webp', '.webp');
+                    // console.log('IMAGE', newImage);
+                    return { 
+                        price_data: {
+                            currency: 'usd',
+                            product_data: {
+                                name: item.name,
+                                images: [newImage],
+                            },
+                            unit_amount: item.price * 100,
+                        }, 
+                        adjustable_quantity: {
+                            enabled: true,
+                            minimum: 1,
+                        },
+                        quantity: item.quantity
                     }
-                ],
-                mode: 'payment',
+                }),
                 success_url: `${req.header.origin}/?success=true`,
                 cancel_url: `${req.headers.origin}/canceled=true`,
             }; 
             const session = await stripe.checkout.sessions.create(params);
-            res.redirect(303, session.url);
+            res.status(200).json(session);
         } catch (err) {
             res.status(err.statusCode || 500).json(err.message);
         }
